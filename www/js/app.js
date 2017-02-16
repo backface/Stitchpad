11324a6b676beaef9f57af72da6be76960fd692f
Michael Aschauer <m@ash.to>
2017-02-16 13:51:11 +0100

    fixing undo

--- www/js/app.js
+++ www/js/app.js
@@ -1,63 +1,3 @@
-document.addEventListener("deviceready", onDeviceReady, false);
-function onDeviceReady() {
-    console.log(cordova.file);
-	console.log(cordova.file.dataDirectory);
-    console.log("we Are on cordova!");
-
-	var errorHandler = function (fileName, e) {
-	    var msg = '';
-
-	    switch (e.code) {
-	        case FileError.QUOTA_EXCEEDED_ERR:
-	            msg = 'Storage quota exceeded';
-	            break;
-	        case FileError.NOT_FOUND_ERR:
-	            msg = 'File not found';
-	            break;
-	        case FileError.SECURITY_ERR:
-	            msg = 'Security error';
-	            break;
-	        case FileError.INVALID_MODIFICATION_ERR:
-	            msg = 'Invalid modification';
-	            break;
-	        case FileError.INVALID_STATE_ERR:
-	            msg = 'Invalid state';
-	            break;
-	        default:
-	            msg = 'Unknown error';
-	            break;
-	    }
-	    console.log('Error (' + fileName + '): ' + msg);
-	};
-
-	saveAs = function(blob, fileName) {
-		console.log("save via cordova file plugin ");
-        //path =  'cdvfile://localhost/sdcard/stitchpad';
-        //path = cordova.file.dataDirectory;
-        persistentFS= cordova.file.externalDataDirectory; //||cordova.file.DataDirectory||fileSystem.root.toURL();
-		window.resolveLocalFileSystemURL(persistentFS, function (directoryEntry) {
-            console.log('directory found (cordova): ' + directoryEntry.toURL());
-              console.log('directory found (native) : ' + directoryEntry.toNativeURL())
-			directoryEntry.getFile(fileName, { create: true }, function (fileEntry) {
-				fileEntry.createWriter(function (fileWriter) {
-					fileWriter.onwriteend = function (e) {
-						// for real-world usage, you might consider passing a success callback
-						console.log('Write of file "' + fileName + '"" to  ' + directoryEntry.toNativeURL() + ' completed.');
-					};
-					fileWriter.onerror = function (e) {
-						// you could hook this up with our global error handler, or pass in an error callback
-						console.log('Write failed: ' + e.toString());
-					};
-					fileWriter.write(blob);
-				}, errorHandler.bind(null, fileName));
-			}, errorHandler.bind(null, fileName));
-		}, errorHandler.bind(null, fileName));
-	};
-
-
-
-}
-
 
 $(function() {
 	createGrid();
@@ -266,6 +206,7 @@ $(function() {
 		turtleShepherd.undoStep();
 		lines.remove(_.last(lines.children));
 		stitches.remove(_.last(stitches.children));
+		lastPos = (_.last(stitches.children).translation);
 	};
 
 	// TODO: Save
@@ -399,3 +340,60 @@ $(function() {
 	}
 
 });
+
+
+// We are on cordova so we overwrite certain functions
+
+
+document.addEventListener("deviceready", onDeviceReady, false);
+function onDeviceReady() {
+    console.log(cordova.file);
+	console.log(cordova.file.dataDirectory);
+    console.log("we Are on cordova!");
+
+	var errorHandler = function (fileName, e) {
+	    var msg = '';
+
+	    switch (e.code) {
+	        case FileError.QUOTA_EXCEEDED_ERR:
+	            msg = 'Storage quota exceeded';
+	            break;
+	        case FileError.NOT_FOUND_ERR:
+	            msg = 'File not found';
+	            break;
+	        case FileError.SECURITY_ERR:
+	            msg = 'Security error';
+	            break;
+	        case FileError.INVALID_MODIFICATION_ERR:
+	            msg = 'Invalid modification';
+	            break;
+	        case FileError.INVALID_STATE_ERR:
+	            msg = 'Invalid state';
+	            break;
+	        default:
+	            msg = 'Unknown error';
+	            break;
+	    }
+	    console.log('Error (' + fileName + '): ' + msg);
+	};
+
+	saveAs = function(blob, fileName) {
+		console.log("save via cordova file plugin ");
+        persistentFS= cordova.file.externalDataDirectory; //||cordova.file.DataDirectory||fileSystem.root.toURL();
+		window.resolveLocalFileSystemURL(persistentFS, function (directoryEntry) {
+			directoryEntry.getFile(fileName, { create: true }, function (fileEntry) {
+				fileEntry.createWriter(function (fileWriter) {
+					fileWriter.onwriteend = function (e) {
+						// for real-world usage, you might consider passing a success callback
+						console.log('Write of file "' + fileName + '"" to  ' + directoryEntry.toNativeURL() + ' completed.');
+					};
+					fileWriter.onerror = function (e) {
+						// you could hook this up with our global error handler, or pass in an error callback
+						console.log('Write failed: ' + e.toString());
+					};
+					fileWriter.write(blob);
+				}, errorHandler.bind(null, fileName));
+			}, errorHandler.bind(null, fileName));
+		}, errorHandler.bind(null, fileName));
+	};
+}
